a5f0fb3e64069e2a3e5944c107bca09eea2aa3d8
minnyww <minrrverse@gmail.com>
2023-12-07 17:18:27 +0700

    [edit] remove log

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import PostList from './components/PostList'
 
 
 export default function Home() {
-  console.log('GITHUB_TOKEN=>', process.env.GITHUB_TOKEN)
   return (
     <div>
       <h1>My Post</h1>
